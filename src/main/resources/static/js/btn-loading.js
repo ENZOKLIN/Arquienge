bcc945c2106f602f076098a2948bb916a5e0be83
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2019-11-30 04:38:00 -0300

    Projeto Arquienge Atualizado com nova view de login.

---
+++ src/main/resources/static/js/btn-loading.js
@@ -0,0 +1,9 @@
+function btnLoadingProprietario(){
+    document.getElementById("btn-p").classList.add("running");
+    document.getElementById("arrow-right-proprietario").style.display = "none";
+}
+
+function btnLoadingEngenheiro(){
+    document.getElementById("btn-e").classList.add("running");
+    document.getElementById("arrow-right-engenheiro").style.display = "none";
+}
