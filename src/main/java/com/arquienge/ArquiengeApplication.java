88cf517e6db3d7490a0768b1e8a8f01bbe705889
ENZOKLIN <enzo_klin@estudante.sc.senai.br>
2019-12-03 06:22:21 -0300

    Alterações para Implantação do Sistema (03-12)

--- src/main/java/com/arquienge/ArquiengeApplication.java
+++ src/main/java/com/arquienge/ArquiengeApplication.java
@@ -2,9 +2,16 @@ package com.arquienge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ArquiengeApplication {
+public class ArquiengeApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ArquiengeApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArquiengeApplication.class, args);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
- insert into administrador(nome,senha,sobrenome,usuario)  values('Arquienge','G0ld3nc0d3','Administrador','Arquienge');
+TRUNCATE TABLE administrador;
+insert into administrador(nome,senha,sobrenome,usuario)  values('Arquienge','G0ld3nc0d3','Administrador','Arquienge');
